c4d116fb347771faedbf3cacf5412a9ede126eb3
Andre Savic <andresavic@icloud.com>
2021-12-16 01:38:35 +0100

    Add streaming client changes

--- index.js
+++ index.js
@@ -1,104 +1,110 @@
 const fs = require('fs');
 const m3u8Parser = require('m3u8-parser');
 const tus = require('tus-js-client');
+var path = require('path');
+
+const { performance } = require('perf_hooks');
 
 const FILE_SERVER = 'http://23.88.119.203:1080/files/';
 //const FILE_SERVER = 'http://0.0.0.0:1080/files/';
 
 class Publisher {
-    constructor(path, playlist) {
-      this.playlist = playlist;
+    constructor(key, path, delay) {
+      this.key = key;
+      this.delay = delay, 
       this.path = path;
+      this.currentPos = 0;
       this.queue = [];
-
       this.uploaded = [];
     }
 
     start() {
-        fs.watchFile(this.path + this.playlist, (curr, prev) => {
-            console.log(`Playlist file Changed`);
-
-            fs.readFile(this.path + this.playlist, 'utf8' , (err, data) => {
-                if (err) {
-                    console.error(err)
-                    return
-                }
-                this.processNewSegment(data);
-            })
-
-        });
-
-    
-    }
-
-
-    processNewSegment(manifest) {
-        var parser = new m3u8Parser.Parser();
-
-        parser.push(manifest);
-        parser.end();
-
-        var parsedManifest = parser.manifest;
-
-        //console.log(parsedManifest);
-
-        parsedManifest.segments.forEach((segment) => {
-            if (this.queue.includes(segment.uri)) {
-                console.log("Segement " + segment.uri + " already in queue");
+        fs.watch(this.path, (event, filename) => {
+            if (path.extname(filename) != '.ts') {
                 return;
             }
 
-            this.queue.push(segment.uri);
-            console.log("Add " + segment.uri + " to upload queue");
-        })
-        
+            this.processNewSegment(filename);
+        });
 
-        if (this.uploaded.length === 0) {
-            this.work(0);
+        this.work();
+    }
+
+
+    processNewSegment(segment) {
+        if (this.queue.includes(segment)) {
+            return;
         }
-        //this.addQueue(manifest);
+
+        this.queue.push(segment);
     }
 
     
 
-    work(pos) {
-        console.log(this.uploaded.length + "/" + this.queue.length);
-        if (this.queue[pos] === undefined) {
+    work() {
+        console.log("Work on: " + this.queue[this.currentPos] + " Stats: " + this.currentPos + " / " + this.queue.length +  " (" + (this.queue.length - this.currentPos)  + ")");
+
+        if (this.queue[this.currentPos] === undefined) {
             setTimeout(() => {
-                this.work(pos);
+                this.work();
             }, 500);
             return;
         }
-        console.log("Work on: " + this.queue[pos]);
-        this.upload(this.queue[pos], (filename) => {
+
+        if (this.uploaded.includes(this.queue[this.currentPos])) {
+            console.log("Already uploaded");
+            this.currentPos++;
+            this.work();
+            return;
+        }
+
+
+        if ((this.queue.length - this.currentPos) <= this.delay) {
+            setTimeout(() => {
+                this.work();
+            }, 100);
+
+            return;
+        }
+
+        let start = performance.now();
+        this.upload(this.queue[this.currentPos], (filename) => {
             this.uploaded.push(filename);
-            console.log('Upload finished: ' + filename);
+            let time = Math.round(performance.now() - start);
+            console.log('Done Pos: ' + this.currentPos +  ' Name:' + filename + ' Time:' + time);
+            this.currentPos++;
             //this.checkManifestFinishedUpload(parsedManifest, manifest);
-            if (this.queue.length > pos) {
-                this.work(pos + 1);
+            if (this.queue.length >= this.currentPos) {
+                this.work();
             }
+        }, (error) => {
+            console.log("ERROR", error);
+            this.work();
         });
     }
 
-    upload(filename, successCallback) {
+    upload(filename, successCallback, errorCallback) {
         let path = this.path + filename;
         var file = fs.createReadStream(path);
         var size = fs.statSync(path).size;
 
         var options = {
             endpoint: FILE_SERVER,
-            retryDelays: [0, 1000, 2000, 4000, 8000, 10000, 15000, 20000, 30000, 40000, 50000, 60000],
+            retryDelays: [0, 1000],
             metadata: {
+              key: this.key,
               filename: filename,
+              currentPos: this.currentPos,
+              queueLength: this.queue.length,
               filetype: 'application/octet-stream',
             },
             uploadSize: size,
             onError (error) {
-              console.log("ERROR", error);
+              errorCallback && errorCallback(error);
             },
             onProgress (bytesUploaded, bytesTotal) {
-              var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
-              console.log(`${percentage}% - ${bytesUploaded}/${bytesTotal}`)
+              //var percentage = (bytesUploaded / bytesTotal * 100).toFixed(2)
+              //console.log(`${percentage}% - ${bytesUploaded}/${bytesTotal}`)
             },
             onSuccess () {
               successCallback && successCallback(filename);
@@ -110,5 +116,5 @@ class Publisher {
     }
 }
 
-const publisher = new Publisher('/Users/andresavic/hls/', 'stream.m3u8');
+const publisher = new Publisher('test3', '/Users/andresavic/hls/', 10);
 publisher.start();
